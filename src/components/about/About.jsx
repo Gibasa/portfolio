fb8979ed5ccb49c44d6bb6e75d6ebc4743be82d3
Gilberto Sá <gibasa@gmail.com>
2024-12-26 14:07:35 -0300

    about text updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -24,24 +24,36 @@ function About() {
         <img src="" alt="" />
       </div>
       <div className="socials">
-        <a href="https://github.com/Gibasa" target="blank"><FontAwesomeIcon className="icon" icon={faGithub} /></a>
-        <a href="https://www.linkedin.com/in/gilberto-sa/" target="blank"><FontAwesomeIcon className="icon" icon={faLinkedin} /></a>
-        <a href="https://wa.me/5571994066099" target="blank"><FontAwesomeIcon className="icon" icon={faWhatsapp} /></a>
+        <a href="https://github.com/Gibasa" target="blank">
+          <FontAwesomeIcon className="icon" icon={faGithub} />
+        </a>
+        <a href="https://www.linkedin.com/in/gilberto-sa/" target="blank">
+          <FontAwesomeIcon className="icon" icon={faLinkedin} />
+        </a>
+        <a href="https://wa.me/5571994066099" target="blank">
+          <FontAwesomeIcon className="icon" icon={faWhatsapp} />
+        </a>
       </div>
       <div className="about-text">
         <h1>GILBERTO SÁ</h1>
-        <p>Hello, welcome! {"I'm"} a Front-end development enthusiast. </p>{" "}
+        <p>Hello, welcome! {"I'm"} a passionate Front-End Developer. </p>{" "}
         <p>
           {" "}
-          My journey in the world of Software Development is driven by the
-          continuous pursuit of growth and improvement.{" "}
+          With a strong focus on HTML, CSS, JavaScript, React, Styled
+          Components, and Material UI, I’m committed to creating responsive,
+          modern, and user-friendly websites and applications.{" "}
         </p>{" "}
         <p>
           {" "}
-          I possess skills in Html, Css, Javascript, Python, Vue.js, React,
-          Node, and MySQL. Eager to embrace new challenges and keep learning.{" "}
+          My journey in Software Development is driven by the continuous pursuit
+          of growth and learning. I thrive on new challenges and aim to deliver
+          high-quality solutions that meet both user needs and business goals.{" "}
         </p>{" "}
-        <p> {"Let's"} connect!</p>
+        <p>
+          {" "}
+          {"Let's"} connect and explore how I can help bring your digital
+          projects to life!
+        </p>
       </div>
       <div
         className={`about-download ${hovered ? "hovered" : ""}`}
@@ -50,8 +62,18 @@ function About() {
       >
         <div className="download">CV DOWNLOAD</div>
         <div className="links">
-          <a href="https://drive.google.com/file/d/15C0oDIAY_vYVIVTvAOaBq914sCkDUXOY/view?usp=drive_link" target="blank">ENGLISH</a>
-          <a href="https://drive.google.com/file/d/1yDt5D-yWRUT_X2_iKkL4EtYTH2EfV1sl/view?usp=drive_link" target="blank">PORTUGUÊS</a>
+          <a
+            href="https://drive.google.com/file/d/15C0oDIAY_vYVIVTvAOaBq914sCkDUXOY/view?usp=drive_link"
+            target="blank"
+          >
+            ENGLISH
+          </a>
+          <a
+            href="https://drive.google.com/file/d/1yDt5D-yWRUT_X2_iKkL4EtYTH2EfV1sl/view?usp=drive_link"
+            target="blank"
+          >
+            PORTUGUÊS
+          </a>
         </div>
       </div>
     </div>
